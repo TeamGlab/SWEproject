79f4e0646c1fecc7fb42cf7e314a7fa0d32692ec
Andrew Lopez <andrewlopez@ufl.edu>
2021-04-15 20:06:58 -0400

    Aesthetic change to email form.

--- UFSailWebsite/Information/forms.py
+++ UFSailWebsite/Information/forms.py
@@ -1,7 +1,11 @@
 from django import forms
+from django.forms import Textarea, TextInput, EmailInput
 from .models import EmailMember
+
 
 class EmailForm(forms.ModelForm):
     class Meta:
         model = EmailMember
-        fields = ["email"]
+        fields = ['email']
+        widgets = {'email': EmailInput(attrs={'style': 'margin-left : 10px'})}
+
