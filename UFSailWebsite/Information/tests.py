88a6d1d689d032440910d1f84675575fc4a859af
London Torres <londonztorres@gmail.com>
2021-04-20 17:08:28 -0400

    Additional test cases

--- UFSailWebsite/Information/tests.py
+++ UFSailWebsite/Information/tests.py
@@ -1,9 +1,23 @@
 from django.test import TestCase, SimpleTestCase
 from django.contrib.auth.models import User
-from .models import Event
+from .models import Officer, Event, to_standard
 from .custom_calendar import Calendar
 from datetime import datetime, timedelta, time
 from .event_provider import DatabaseEventProvider
+
+class TestOfficer(TestCase):
+	def setUp(self):
+		self.testOfficer = Officer(firstname='First', lastname='Last', 
+								role='President', email='test@email.com', 
+								bio='Bio')
+		self.testOfficer.save()
+
+	def test__str__(self):
+		assert(str(self.testOfficer) == "First Last: President")
+
+	def testFullName(self):
+		assert(self.testOfficer.fullname() == "First Last")
+
 
 class TestCalendar(TestCase):
 	
@@ -46,3 +60,13 @@ class TestCalendar(TestCase):
 		assert(e.get_time_string() == "2:30 AM to 3:30 AM")
 		assert(e.calendar_display_time() == "2:30AM")
 
+class SimpleTests(SimpleTestCase):
+	def testTo_Standard(self):
+		hour1 = 24
+		hour2 = 12
+		hour3 = 3
+		hour4 = 18
+		assert(to_standard(hour1) == 12)
+		assert(to_standard(hour2) == 12)
+		assert(to_standard(hour3) == 3)
+		assert(to_standard(hour4) == 6)
